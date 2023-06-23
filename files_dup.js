008d6bf21237b606d08da004788f17a4a54f31cf
Juan David Carvajal Cucuñame <72934174+JuanDavid1321@users.noreply.github.com>
2023-06-23 18:37:45 -0500

    Add files via upload

---
+++ files_dup.js
@@ -0,0 +1,10 @@
+var files_dup =
+[
+    [ "comunicaciones.cpp", "comunicaciones_8cpp.html", "comunicaciones_8cpp" ],
+    [ "comunicaciones.h", "comunicaciones_8h.html", "comunicaciones_8h" ],
+    [ "mivUniSalud.ino", "miv_uni_salud_8ino.html", "miv_uni_salud_8ino" ],
+    [ "reloj.cpp", "reloj_8cpp.html", "reloj_8cpp" ],
+    [ "reloj.h", "reloj_8h.html", "reloj_8h" ],
+    [ "sensor.cpp", "sensor_8cpp.html", "sensor_8cpp" ],
+    [ "sensor.h", "sensor_8h.html", "sensor_8h" ]
+];
---
+++ reloj_8h.js
@@ -0,0 +1,10 @@
+var reloj_8h =
+[
+    [ "actualizarRTC", "reloj_8h.html#a3be46386af3f2ed98ac913af446fe077", null ],
+    [ "construirURLDatos", "reloj_8h.html#ae4d6ae87a5d8e6f562809993e2435b16", null ],
+    [ "entrarEnDeepSleep", "reloj_8h.html#a0ac7b3299e30aee1c5e41c90f409555e", null ],
+    [ "daylightOffset_sec", "reloj_8h.html#abcecff6206d272c30174ca268e2e6f0a", null ],
+    [ "gmtOffset_sec", "reloj_8h.html#a6c7db28543c3b2ef692f8dcb63964f2b", null ],
+    [ "ntpServer", "reloj_8h.html#a6bd950625db0f240acc63a9cb41873f6", null ],
+    [ "rtc", "reloj_8h.html#a9914c8ecdaedd4bd9c93c344bea096c0", null ]
+];
